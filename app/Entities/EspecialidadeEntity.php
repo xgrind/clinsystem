3a82e99a4fe09533ff773605a0485c0ef7847267
Michael Martins <michaelfm21@gmail.com>
2023-03-19 00:31:01 -0300

    adição de campos de especialidade

--- app/Entities/EspecialidadeEntity.php
+++ app/Entities/EspecialidadeEntity.php
@@ -6,6 +6,15 @@ use CodeIgniter\Entity\Entity;
 
 class EspecialidadeEntity extends Entity
 {
+    protected $attributes = [
+        'id' => null,
+        'nome' => null,
+        'descricao' => null,
+        'ativo' => 's',
+        'created_at' => null,
+        'updated_at' => null,
+        'deleted_at' => null,
+    ];
     protected $datamap = [];
     protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
     protected $casts   = [];
