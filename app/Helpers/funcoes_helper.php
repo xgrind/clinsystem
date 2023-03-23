b17fad7cbc15a8cfd8d1c3411c87eab52c23a362
Michael Martins <michaelfm21@gmail.com>
2023-03-22 22:03:48 -0300

    helper funcoes

---
+++ app/Helpers/funcoes_helper.php
@@ -0,0 +1,48 @@
+<?php
+
+function getEstados()
+{
+    $estadosBrasileiros = [
+        '' => '-- Selecione --',
+        'AC'=>'Acre',
+        'AL'=>'Alagoas',
+        'AP'=>'Amapá',
+        'AM'=>'Amazonas',
+        'BA'=>'Bahia',
+        'CE'=>'Ceará',
+        'DF'=>'Distrito Federal',
+        'ES'=>'Espírito Santo',
+        'GO'=>'Goiás',
+        'MA'=>'Maranhão',
+        'MT'=>'Mato Grosso',
+        'MS'=>'Mato Grosso do Sul',
+        'MG'=>'Minas Gerais',
+        'PA'=>'Pará',
+        'PB'=>'Paraíba',
+        'PR'=>'Paraná',
+        'PE'=>'Pernambuco',
+        'PI'=>'Piauí',
+        'RJ'=>'Rio de Janeiro',
+        'RN'=>'Rio Grande do Norte',
+        'RS'=>'Rio Grande do Sul',
+        'RO'=>'Rondônia',
+        'RR'=>'Roraima',
+        'SC'=>'Santa Catarina',
+        'SP'=>'São Paulo',
+        'SE'=>'Sergipe',
+        'TO'=>'Tocantins'
+    ];
+
+    return $estadosBrasileiros;
+}
+
+function getTipo($tipo)
+{
+    switch ($tipo) {
+        case 'adm': return 'Administrador';
+        case 'sec': return 'Secretário';
+        case 'med': return 'Médico';
+        case 'pac': return 'Paciente';
+        default: return 'Erro';
+    }
+}
